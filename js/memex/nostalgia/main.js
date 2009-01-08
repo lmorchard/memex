1eab75e4722e3599209ceb5109b4d1b1f45d9dc3
Leslie Michael Orchard <l.m.orchard@pobox.com>
2009-01-07 20:29:48 -0500

    Moving the form element class name munging into init rather than onload

--- js/memex/nostalgia/main.js
+++ js/memex/nostalgia/main.js
@@ -7,16 +7,22 @@ memex.nostalgia.main = function() {
     return {
 
         init: function() {
-            dojo.addOnLoad(this, 'onLoad');
-        },
 
-        onLoad: function() {
-
+            // Toss classes onto form elements based on their types.
+            // HACK: This assumes this is loaded at the end of the document,
+            // and enough of the DOM is already available.
             // see: http://www.dustindiaz.com/input-element-css-woes-are-over/
             dojo.query('input[type]').forEach(function(el) {
                 dojo.addClass(el, dojo.attr(el, 'type'));
             }, this);
 
+            dojo.addOnLoad(this, 'onLoad');
+        },
+
+        onLoad: function() {
+
+            // Flash some paragraphs for attention, generally messages
+            // from forms.
             dojo.query('p.hint').forEach(function(el) {
                 dojo.animateProperty({
                     node: el, duration: 1000, properties: {
